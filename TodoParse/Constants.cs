43324ef3b619d6f2544201e389a243d5f17fef14
Steven Taylor Ramzel <steven.ramzel@elementum.com>
2015-05-22 15:40:26 -0700

    Initial commit

---
+++ TodoParse/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodoXaml
+{
+	public static class Constants
+	{
+		// https://www.parse.com/apps/YOUR_APP_NAME/edit#settings
+		public static string ApplicationId = "5V6uI4uhzAzzKc4NMIHfyHVjqi6FFBW6eyi3Yy8o"; // APPLICATION ID
+		public static string Key = "3TfG5T3ZdXiLruJSWzpUDhqw2NA3czosJ5uO0yHS"; // .NET KEY
+	}
+}
+
